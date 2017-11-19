fa54ecb7f7b565fff27c897a7d4258bfef60a371
Kohei Yamamoto <kh2yam1@gmial.com>
2017-11-19 18:20:50 -0500

    [add] Add visualize functions

--- ut.py
+++ ut.py
@@ -17,9 +17,32 @@ def remove_duplication(indv_list):
   return nodupl_list
 
 
-def pick_up_best_indvs(indv_list, selection="pareto"):
+def calc_nvehicle_average(indv_list):
+    avg = 0
+    for indv in indv_list:
+        avg += indv.get_nvehicle()
+    return avg/len(indv_list)
+
+def calc_distance_average(indv_list):
+    avg = 0
+    for indv in indv_list:
+        avg += indv.distance
+    return avg/len(indv_list)
+
+def pick_up_best_indvs(switch, indv_list):
     best_solutions = []
     if selection == "pareto":
         (best_solutions, others) = fnc.make_current_ranking_list(indv_list)
         others = []
+    elif switch in ["wsum", "ranksum"]
+        best_indv = indv_list[0]
+        for indv in indv_list:
+            if indv.fitness < best_indv.fitness:
+                best_indv = indv
+        best_solutions = [best_indv]
+    else:
+        print("!!!!! [ut/pick_up_best_indvs] switch doesn't has such paramerter:", \
+                switch, "!!!!!")
+        sys.exit()
+
     return remove_duplication(best_solutions)
---
+++ vistools.py
@@ -0,0 +1,94 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def _plot(filename, title, x, y1, y2=[], \
+        xlabel, ylabel, label1, lebel2):
+    plt.clf()
+    plt.plot(x, y1, label=label1)
+    if len(y2) != 0:
+        plt.plot(x, y2, label=label2)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.savefig(filename)
+
+def plot_graphs(path="", generations, nvehicle_avgs, \
+        distance_avgs, nvehicle_bests, distance_bests):
+    if len(path) != 0:
+        path = path + "/"
+    # Avarage of Num of Vehicles by Generations
+    title = "Avarage of Number of Vehicles by Generations"
+    _plot(path+"vehicles_avg.png", title, generations, nvehicle_avgs, y2=[], \
+            "Genration", "Avarage of Number of Vehicle", \
+            "Vehicles", "")
+    # Avarage of Distance by Generations
+    title = "Avarage of Distance by Generations"
+    _plot(path+"distance_avg.png", title, generations, distance_avgs, y2=[], \
+            "Genration", "Avarage of Distance", \
+            "Distance", "")
+    # Best Num of Vehicles by Generations
+    title = "Best Number of Vehicles by Generations"
+    _plot(path+"vehicles_best.png", title, generations, nvehicle_bests, y2=[], \
+            "Genration", "Best Number of Vehicle", \
+            "Vehicles", "")
+    # Best Distance by Generations
+    title = "Avarage of Distance by Generations"
+    _plot(path+"distance_best.png", title, generations, distance_bests, y2=[], \
+            "Genration", "Avarage of Distance", \
+            "Distance", "")
+    # Num of Vehicles (Avarage & Best) by Generations
+    title = "Number of Vehicles by Generations"
+    _plot(path+"vehicles_avg_best.png", title, generations, nvehicle_avg, \
+            nvehicle_bests, "Genration", "Number of Vehicle", \
+            "Avarage of Vehicles", "Best Vehicles")
+    # Distance (Avarage & Best) by Generations
+    title = "Distance by Generations"
+    _plot(path+"distance_avg_best.png", title, generations, distance_avg, \
+            distance_bests, "Genration", "Avarage of Distance", \
+            "Avarage of Distance", "Best Distance")
+
+def draw_routings(path="", nodes, solutions):
+    G = nx.Graph()
+    depot = nodes.get_depot()
+    colors = ['black', 'green', 'cyan', 'magenta', 'red', 'blue']
+  
+    # Add nodes
+    G.add_node(depot.get_id(), pos=depot.get_pos(), color='blue')
+    for customer in nodes.get_customers():
+      G.add_node(customer.get_id(), pos=customer.get_pos(), color='red')
+  
+    for (index, solution) in enumerate(solutions):
+      edge_list = []
+      G.remove_edges_from(list(G.edges()))
+      plt.clf()
+  
+      for route in solution.chromosome:
+        edges = []
+        edges.append((depot.get_id(), route[0]))
+        for i in range(len(route)-1):
+          edges.append((route[i], route[i+1]))
+        edges.append((route[-1], depot.get_id()))
+        edge_list.append(edges)
+  
+      for edges in edge_list:
+        G.add_edges_from(edges)
+  
+      # Draw Nodes
+      positions = {id_: (x, y) for (id_, (x, y)) in nx.get_node_attributes(G, 'pos').items()}
+      nx.draw(G, positions, with_labels=True)
+      nx.draw_networkx_nodes(G,positions,
+                             nodelist=[depot.get_id()],
+                             node_color='blue',)
+      nx.draw_networkx_nodes(G,positions,
+                             nodelist=nodes.get_customers_id_list(),
+                             node_color='r',)
+      # Draw Edges
+      for (i, edges) in enumerate(edge_list):
+        nx.draw_networkx_edges(G,positions,
+                               edgelist=edges,
+                               edge_color=colors[i%(len(colors))])
+  
+    if len(path) != 0:
+        path = path + "/"
+    plt.savefig(path + "routing" + str(index).zfill(3) + ".png")
