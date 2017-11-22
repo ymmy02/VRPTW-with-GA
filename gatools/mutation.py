89780eab832e4221aa08173601eda7b1bb8ade09
Kohei Yamamoto <kh2yam1@gmial.com>
2017-11-22 14:58:07 -0500

    [change] Change the structure of mutation

--- gatools/mutation.py
+++ gatools/mutation.py
@@ -26,27 +26,16 @@ def _random_choice_and_reinsert(chromosome, nodes):
         j = random.choice(range(len(chromosome[i])+1))
     chromosome[i].insert(j, insert_node)
 
-def _insrt(nodes, chromosome, rate=0.02):
+def _insertion(nodes, chromosome, rate=0.02):
     size = len(ut.flatten(chromosome))
     for _ in range(size):
         if random.random() < rate:
             _random_choice_and_reinsert(chromosome, nodes)
-            chromosome = fnc.remove_null_route(chromosome)
     return chromosome
-
-def _insertion(nodes, offsprings, rate, irate):
-    new_offsprings = []
-    for indv in offsprings:
-        tmp = copy.deepcopy(indv)
-        if random.random() < rate:
-            tmp.chromosome = _insrt(nodes, indv.chromosome, irate)
-        tmp.chromosome = fnc.remove_null_route(tmp.chromosome)    # Remove the route which has no nodes
-        new_offsprings.append(tmp)
-    return new_offsprings
 #=====</Insersion>=====#
 
 #=====<Inversion>=====#
-def _inv(nodes, chromosome):
+def _inversion(nodes, chromosome):
     flat_route = ut.flatten(chromosome)
     size = len(flat_route)
     cut1 = random.randint(0, size-1)
@@ -57,16 +46,6 @@ def _inv(nodes, chromosome):
             + flat_route[cut2:size]
     new_chromosome = fnc.shape_flat_to_vehicles(nodes, new_flat_route)
     return new_chromosome
-
-def _inversion(nodes, offsprings, rate):
-  new_offsprings = []
-  for indv in offsprings:
-    tmp = copy.deepcopy(indv)
-    if random.random() < rate:
-      tmp.chromosome = _inv(nodes, indv.chromosome)
-    tmp.chromosome = fnc.remove_null_route(tmp.chromosome)    # Remove the route which has no nodes
-    new_offsprings.append(tmp)
-  return new_offsprings
 #=====</Inversion>=====#
 
 
@@ -75,14 +54,19 @@ def _inversion(nodes, offsprings, rate):
 ##########
 def done(switch, nodes, offsprings, rate=0.3, irate=0.02):
     Timer.start("mutation")
-    if switch == "insersion":
-        new_offsprings =  _insertion(nodes, offsprings, rate, irate)
-    elif switch == "inversion":
-        new_offsprings =  _inversion(nodes, offsprings, rate)
-    else:
-        print("!!!!! [mutation/done] switch doesn't has such paramerter:", \
-                switch, "!!!!!")
-        sys.exit()
+    new_offsprings = []
+    for indv in offsprings:
+        tmp = copy.deepcopy(indv)
+        if random.random() < rate:
+            if switch == "insersion":
+                tmp.chromosome = _insertion(nodes, indv.chromosome)
+            elif switch == "inversion":
+                tmp.chromosome = _inversion(nodes, indv.chromosome)
+            else:
+                print("!!!!! [mutation/done] switch doesn't has such paramerter:", \
+                        switch, "!!!!!")
+                sys.exit()
+        tmp.chromosome = fnc.remove_null_route(tmp.chromosome)    # Remove the route which has no nodes
+        new_offsprings.append(tmp)
     Timer.end("mutation")
-
     return new_offsprings
