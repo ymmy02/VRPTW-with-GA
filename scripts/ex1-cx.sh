8cc5212f65533db3bb967d0a02a852fb14d17123
Kohei Yamamoto <kh2yam1@gmial.com>
2017-12-04 11:03:35 -0500

    [change] Change script name and contents

---
+++ scripts/ex1-cx.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#========================
+DATASETDIR=dataset
+#TYPE=R1
+#INDEX=1
+# input file is ${TYPE}0${INDEX}.txt ex) R101.txt
+RESULTDIR=results
+POPULATION=100
+GENERATION=100
+SELECTION=wsum       # pareto, wsum, ranksum
+#CROSSOVER=bcrc      # uox, pmx, rc, bcrc
+MUTATION=inversion  # inversion, insersion
+W_NVWHICLE=100
+W_DISTANCE=0.001
+ELITE=0
+TOURNAMENT=3
+CXRATE=0.6
+MURATE=0.2
+MUIRATE=0.03
+SUFFIX=
+#========================
+
+if [ $# -ne 1 ]; then
+    echo "[usage] $0 [uox, pmx, bcrc]" 1>&2
+    exit 1
+fi
+CROSSOVER=${1}      # uox, pmx, bcrc
+
+cd ../
+
+python3 setup.py build_ext --inplace
+
+for TYPE in R1 C1 RC1 R2 C2 RC2
+do
+    for INDEX in `seq 1 3`
+    do
+        for CXRATE in 0.6 0.7 0.8 0.9
+        do
+            for i in `seq 1 10`
+            do
+                FILE=${TYPE}0${INDEX}
+                INPUT=${DATASETDIR}/${TYPE}/${FILE}.txt
+                OUTPUT=${RESULTDIR}/ex1/${CROSSOVER}/${FILE}/${CXRATE}
+
+                mkdir -p ${OUTPUT}
+
+                echo "${TYPE}0${INDEX}"
+                python3 main.py ${INPUT} ${OUTPUT} ${POPULATION} \
+                    ${GENERATION} ${SELECTION} ${CROSSOVER} ${MUTATION} \
+                    ${W_NVWHICLE} ${W_DISTANCE} ${ELITE} ${TOURNAMENT} \
+                    ${CXRATE} ${MURATE} ${MUIRATE} ${i}
+            done
+        done
+    done
+done
